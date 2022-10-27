a42b339a1599597dbf3e254db9d41310bfa71bf4
Dykyi Maksym <108894931+DykyiMaxim@users.noreply.github.com>
2022-10-27 16:32:19 -0400

    Working on signup design.Add font's

--- app/src/main/java/com/wm/bitco/ui/Signup.kt
+++ app/src/main/java/com/wm/bitco/ui/Signup.kt
@@ -3,21 +3,29 @@ package com.wm.bitco.ui
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.font.Typeface
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.core.content.res.ResourcesCompat
 import com.wm.bitco.R
 
 @Composable
+@Preview
 fun SignUpForm(){
 
-    Box(
+    Column  (
+
         modifier = Modifier
             .fillMaxSize()
             .background(
@@ -28,8 +36,16 @@ fun SignUpForm(){
                     )
                 )
             )
+    ,horizontalAlignment = Alignment.CenterHorizontally
+
     ){
-        Image(modifier = Modifier.align(Alignment.Center),imageVector = ImageVector.vectorResource(id = R.drawable.logo), contentDescription = "logo")
+            Spacer(modifier = Modifier.padding(48.dp))
+
+            Image(modifier = Modifier,imageVector = ImageVector.vectorResource(id = R.drawable.logo), contentDescription = "logo")
+            Spacer(modifier = Modifier.padding(16.dp))
+            Text(text = "Get Started",style = MaterialTheme.typography.body2.copy(color = Color(0xFF3D1273)))
+        }
+
 
 
     }
@@ -37,7 +53,7 @@ fun SignUpForm(){
 
 
 
-}
+
 
 
 
--- app/src/main/java/com/wm/bitco/ui/theme/Type.kt
+++ app/src/main/java/com/wm/bitco/ui/theme/Type.kt
@@ -1,18 +1,38 @@
 package com.wm.bitco.ui.theme
 
 import androidx.compose.material.Typography
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.wm.bitco.R
 
 // Set of Material typography styles to start with
+val InerFamily =  FontFamily(
+    Font(R.font.inter_black),
+    Font(R.font.inter_bold),
+    Font(R.font.inter_extra_light),
+    Font(R.font.inter_light),
+    Font(R.font.inter_medium),
+    Font(R.font.inter_regular),
+)
+
 val Typography = Typography(
     body1 = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp
+    ),
+    body2= TextStyle(
+        fontFamily = InerFamily,
+        fontWeight = FontWeight.Bold,
+        color = Color.Black,
+        fontSize = 22.sp
+
     )
+
     /* Other default text styles to override
     button = TextStyle(
         fontFamily = FontFamily.Default,
